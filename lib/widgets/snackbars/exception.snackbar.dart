db7f865ffd0b599f1cc7c07466da5d793dc042cb
Evgenii Shcherbakov <iipekolict@gmail.com>
2023-03-08 02:38:14 +0300

    refactor: exception snackbar refactoring

--- lib/widgets/snackbars/exception.snackbar.dart
+++ lib/widgets/snackbars/exception.snackbar.dart
@@ -5,13 +5,14 @@ class ExceptionSnackbar extends SnackBar {
 
   ExceptionSnackbar(this.message, {super.key})
       : super(
-            backgroundColor: Colors.deepOrange,
-            content: Text(message, style: const TextStyle(color: Colors.white)),
-            action: SnackBarAction(
-              label: 'Скрыть',
-              textColor: Colors.black54,
-              onPressed: () {},
-            ));
+          backgroundColor: Colors.deepOrange,
+          content: Text(message, style: const TextStyle(color: Colors.white)),
+          action: SnackBarAction(
+            label: 'Скрыть',
+            textColor: Colors.black54,
+            onPressed: () {},
+          ),
+        );
 
   static void show(String message, BuildContext context) {
     ScaffoldMessenger.of(context).showSnackBar(ExceptionSnackbar(message));
